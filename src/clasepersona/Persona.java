47295b66fe4db89cd329bd04ce90b15cd2e5fdf5
davidAguilera98 <11236467@alu.murciaeduca.es>
2023-05-05 11:54:08 +0200

    Ejercicio1 JuezLTI

--- src/clasepersona/Persona.java
+++ src/clasepersona/Persona.java
@@ -1,5 +1,98 @@
 package clasepersona;
 
+import java.time.DateTimeException;
+import java.time.LocalDate;
+
 public class Persona {
-    
+
+    private String nombre;
+    private String apellidos;
+    private LocalDate fechaNacimiento;
+
+    public Persona(String nombre, String apellidos) {
+        if ("".equals(nombre) || "".equals(apellidos)) {
+            throw new IllegalArgumentException();
+
+        } else {
+            this.nombre = nombre;
+            this.apellidos = apellidos;
+        }
+    }
+
+    public Persona(String nombre, String apellidos, String fechaNacimiento) throws IllegalArgumentException {
+
+        if ("".equals(nombre) || "".equals(apellidos)) {
+            throw new IllegalArgumentException();
+
+        } else {
+            this.nombre = nombre;
+            this.apellidos = apellidos;
+            this.fechaNacimiento = generarFecha(fechaNacimiento);
+        }
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    /**
+     *
+     * @param separador
+     * @return
+     */
+    public String getFechaNacimiento(char separador) {
+        String fecha = null;
+
+        if (separador != '-' && separador != '/') {
+            throw new IllegalArgumentException();
+
+        } else {
+            if (this.fechaNacimiento != null) {
+                fecha = String.format("%02d%c%02d%c%04d", this.fechaNacimiento.getDayOfMonth(), separador,
+                        this.fechaNacimiento.getMonthValue(), separador, this.fechaNacimiento.getYear());
+            }
+            return fecha;
+        }
+
+    }
+
+    public String getFechaNacimiento() {
+        return getFechaNacimiento('-');
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) throws IllegalArgumentException {
+        this.fechaNacimiento = generarFecha(fechaNacimiento);
+    }
+
+    private LocalDate generarFecha(String fecha) {
+
+        int dia;
+        int mes;
+        int anyo;
+
+        if (!fecha.matches("[0-9]{2}[-][0-9]{2}[-][0-9]{4}")
+                && !fecha.matches("[0-9]{2}[/][0-9]{2}[/][0-9]{4}")) {
+            throw new IllegalArgumentException();
+        } else {
+            try {
+                dia = Integer.parseInt(fecha.subSequence(0, 2).toString());
+                mes = Integer.parseInt(fecha.subSequence(3, 5).toString());
+                anyo = Integer.parseInt(fecha.subSequence(6, fecha.length()).toString());
+
+                return LocalDate.of(anyo, mes, dia);
+            } catch (NumberFormatException ex1) {
+                throw new IllegalArgumentException();
+            } catch (DateTimeException ex2) {
+                throw new IllegalArgumentException();
+            }
+
+        }
+
+    }
+
 }
